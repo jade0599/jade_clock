44ae90baaa32e02a6f54e6c5851717239776fc70
toba75 <gr@jagamax.com>
2020-12-28 13:18:10 +0100

    root to alarms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'alarms#index'
   resources :alarms do
   end
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
